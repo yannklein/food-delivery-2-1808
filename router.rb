4d55ffcd944de8509d7c649db55f7b75d2a7ecd2
Noemi Ashizuka <noemi.calanducci@gmail.com>
2024-10-21 13:51:29 +0900

    initial commit

---
+++ router.rb
@@ -0,0 +1,41 @@
+class Router
+  def initialize
+    @running = true
+  end
+
+  def run
+    while @running
+      print_menu
+      choice = gets.chomp.to_i
+      print `clear`
+      route_action(choice)
+    end
+  end
+
+  private
+
+  def print_menu
+    puts "--------------------"
+    puts "------- MENU -------"
+    puts "--------------------"
+    puts "1. Add new meal"
+    puts "2. List all meals"
+    # puts "3. Add new customer"
+    # puts "4. List all customers"
+    puts "8. Exit"
+    print "> "
+  end
+
+  def route_action(choice)
+    case choice
+    when 1 then 
+
+    when 8 then stop!
+    else puts "Try again..."
+    end
+  end
+
+  def stop!
+    @running = false
+  end
+end
